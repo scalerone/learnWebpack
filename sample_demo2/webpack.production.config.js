edb1e1fbf61e9ef67e5a8fef9888bb5f79b10272
scalerone <676119236@qq.com>
2017-05-09 16:34:18 +0800

    分离app.js和第三方库

--- sample_demo2/webpack.production.config.js
+++ sample_demo2/webpack.production.config.js
@@ -8,13 +8,18 @@ var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 
 module.exports = {
-    // devtool: 'eval-source-map',
+  
   //项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
-  entry: APP_PATH,
+  //分离app.js和第三方库
+  entry: {
+    app: path.resolve(APP_PATH, 'index.js'),
+    //添加要打包在vendors里面的库
+    vendors: ['jquery', 'moment']
+  },
   //输出的文件名 合并以后的js会命名为bundle.js
   output: {
     path: BUILD_PATH,
-    filename: 'bundle.js'
+    filename: '[name].js'
   },
   module: {
      
@@ -40,18 +45,27 @@ module.exports = {
     ]
    
   },
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-  },
-
+  // devServer: {
+  //   historyApiFallback: true,
+  //   hot: true,
+  //   inline: true,
+  // },
 
   plugins: [
+
+
+  //这个使用uglifyJs压缩你的js代码
+   new webpack.optimize.UglifyJsPlugin({minimize: true}),
+   //把入口文件里面的数组打包成verdors.js
+     new webpack.optimize.CommonsChunkPlugin({ name: 'vendors', filename: 'vendors.js' }),
     //添加我们的插件 会自动生成一个html文件
     new HtmlwebpackPlugin({
       title: 'Hello World app'
     })
-     
+     /*new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "window.jQuery": "jquery"
+    })*/
   ]
 };
