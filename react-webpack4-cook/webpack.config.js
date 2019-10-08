8052e2b00dd38186cfee7006cac8839fd84ae6c7
scalerone <676119236@qq.com>
2019-10-08 22:06:57 +0800

    add babel to parse js and jsx

--- react-webpack4-cook/webpack.config.js
+++ react-webpack4-cook/webpack.config.js
@@ -1,22 +1,41 @@
-// webpack.config.js初始化内容
-const path = require('path');
+const path = require("path");
 module.exports = {
     mode: "development",
-    entry: ["./src/index.js"],
+    entry: "./src/index.js",
     output: {
         // 输出目录
         path: path.join(__dirname, "dist"),
         // 文件名称
         filename: "bundle.js"
     },
-    module:{},
-    plugins:[],
+    module: {
+        rules: [
+            {
+                // @babel/core-babel核心模块    @babel/preset-env-编译ES6等 @babel/preset-react-转换JSX
+                // yarn add babel-loader @babel/core @babel/plugin-transform-runtime  @babel/preset-env  @babel/preset-react y-D
+                // @babel/plugin-transform-runtime: 避免 polyfill 污染全局变量，减小打包体积
+                // @babel/polyfill: ES6 内置方法和函数转化垫片
+                // yarn add @babel/polyfill @babel/runtime -D
+
+
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            // 指定babel预处理转义
+                            presets: ["@babel/preset-env", "@babel/preset-react"]
+                        }
+                    }
+                ]
+            }
+        ]
+    },
     devServer:{
-            contentBase:path.join(__dirname,'./dist'),
-            host:'0.0.0.0',
-            port:8080,
-            historyApiFallback:true, // 所有的404都连接到index.html
-
+        contentBase:path.join(__dirname,'./dist'),
+        host:'0.0.0.0',
+        port:8080,
+        historyApiFallback:true, // 所有的404都连接到index.html
     }
-}
-
+};
